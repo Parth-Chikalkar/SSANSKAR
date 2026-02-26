f4360d4642a90568cd40e5b6048430605e1962d9
Parth-Chikalkar <parthchikalkar56@gmail.com>
2026-02-27 00:27:07 +0530

    Added A small Favicon

--- Client/src/Data/Video.js
+++ Client/src/Data/Video.js
@@ -1,2 +1,2 @@
-export default videoLink = "https://ozf1yx5t6oecfmx3.public.blob.vercel-storage.com/SSANSKAR_VID.mp4";
+export default videoLink = "https://ozf1yx5t6oecfmx3.public.blob.vercel-storage.com/SanskarAcademy.mp4";
 
